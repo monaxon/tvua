9a2c655d8e685430e317b5ff19f1ca961801703f
Money Azizjahan <maniazizjahan@gmail.com>
2019-06-01 16:53:21 +0430

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+// import { render } from "react-snapshot";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "App";
+import "index.css";
+import * as serviceWorker from "serviceWorker";
 
+// render(<App />, document.getElementById("app"));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
